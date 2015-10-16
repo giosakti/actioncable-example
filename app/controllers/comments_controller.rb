b60cd56cbfd2f7f52d8fc8aa31b28baec6fdbd58
Giovanni Sakti <giosakti@starqle.com>
2015-10-16 08:59:22 +0700

    integrate comment into article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,7 +29,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
+        format.html { redirect_to :back, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
