650c724f04710e64cf454dcf80c49ff61756ac9b
Giovanni Sakti <giosakti@starqle.com>
2015-10-16 07:59:00 +0700

    article scaffold -- rails g scaffold Article title:string body:text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016004642) do
+ActiveRecord::Schema.define(version: 20151016005345) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
